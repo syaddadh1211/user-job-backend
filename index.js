77657d88c16016426b5e86676fb02c02fcbc2e53
Syaddad Bahalwan <syaddad.bahalwan@realworld-systems.com>
2023-08-18 02:20:06 +0700

    update restAPI get with params

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const express = require("express");
 const app = express();
 const userRoute = require("./routes/users");
 
-let port = process.env.PORT || 3000;
+let PORT = process.env.PORT || 3000;
 
 //routes
 app.use("/user", userRoute);
 
-app.listen(port, () => {
-  console.log("Server is listening on port : " + port);
+app.listen(PORT, () => {
+  console.log("Server is listening on port : " + PORT);
 });
-
--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const pool = require("../db/db");
-const cors = require('cors');
+const cors = require("cors");
 
 const corsOption = {
-  origin: ['http://localhost:3000'],
+  origin: ["http://localhost:3000"],
 };
 
-router.use(cors())
+router.use(cors());
 router.use(express.json()); // req.body
-router.use(express.urlencoded({ extended: true }))
+router.use(express.urlencoded({ extended: true }));
 
 //get all
-router.get("/",  async (req, res) => {
-  
+router.get("/", async (req, res) => {
   try {
-    let query ="SELECT js.id, js.sector_name, st.id type1_id , st.type_name type1, st2.id type2_id, st2.type2_name type2, st3.id type3_id, st3.type_name type3 FROM job_sector js"
-    query = query + " LEFT JOIN sector_type1 st ON js.id = st.job_sector_id LEFT JOIN sector_type2 st2 ON st.id = st2.type1_id"
-    query = query + " LEFT JOIN sector_type3 st3 ON st2.id = st3.type2_id ORDER BY js.sector_name,type1, type2, type3"
-     
+    let query =
+      "SELECT js.id, js.sector_name, st.id type1_id , st.type_name type1, st2.id type2_id, st2.type2_name type2, st3.id type3_id, st3.type_name type3 FROM job_sector js";
+    query =
+      query +
+      " LEFT JOIN sector_type1 st ON js.id = st.job_sector_id LEFT JOIN sector_type2 st2 ON st.id = st2.type1_id";
+    query =
+      query +
+      " LEFT JOIN sector_type3 st3 ON st2.id = st3.type2_id ORDER BY js.sector_name,type1, type2, type3";
+
     const allJobs = await pool.query(query);
     res.json(allJobs.rows);
   } catch (err) {
@@ -26,53 +30,44 @@ router.get("/",  async (req, res) => {
   }
 });
 
+//get result
+router.get("/result", async (req, res) => {
+  try {
+    const name = req.query.name;
+    let query =
+      "select uj.sectortype_id, st.type_name from user_job uj, sector_type1 st where uj.sectortype_id = st.id and uj.name=$1 UNION";
+    query =
+      query +
+      " select uj.sectortype_id, st2.type2_name from user_job uj, sector_type2 st2 where uj.sectortype_id = st2.id and uj.name=$1 UNION";
+    query =
+      query +
+      " select uj.sectortype_id, st3.type_name from user_job uj, sector_type3 st3 where uj.sectortype_id = st3.id and uj.name=$1";
 
-//get one
-// router.get("/:id", async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const user = await pool.query("Select * from public.user where id=$1", [
-//       id,
-//     ]);
-//     res.json(user.rows[0]);
-//   } catch (err) {
-//     console.error(err.message);
-//   }
-// });
+    const allJobs = await pool.query(query, [name]);
+    res.json(allJobs.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 // insert
 router.post("/", (req, res) => {
   try {
     //await
     const { name, selected } = req.body;
-    
+
     selected.map((item, index) => {
       const newUser = pool.query(
         "insert into public.user_job (name, sectortype_id, term_agree) values ($1,$2,$3) returning *",
         [name, item, "1"]
       );
-    })
-    
-    res.json(newUser.rows[0]);
+    });
+
+    res.json({ response: "success" });
   } catch (err) {
     console.error(err.message);
   }
 });
-
-//can update location and gmt_offset
-// router.put("/:id", async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const { location, gmt_offset } = req.body;
-//     const updateUser = await pool.query(
-//       "Update public.user set location=$1, gmt_offset=$2 where id=$3",
-//       [location, gmt_offset, id]
-//     );
-//     res.json("User was updated");
-//   } catch (err) {
-//     console.error(err.message);
-//   }
-// });
 
 //delete
 // router.delete("/:id", async (req, res) => {
